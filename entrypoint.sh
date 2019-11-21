0ec427d033573b9724575866681fcb4c4932d36e
Jan Wozniak <wozniak.jan@gmail.com>
2019-11-21 09:37:15 +0100

    install essential software

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 # Ensure all variables are present
 SOURCE="$1"
@@ -11,7 +11,7 @@ GIT_USER="$5"
 GIT_EMAIL="$6"
 EXCLUDES=()
 if [[ ! -z ${7+x} ]]; then
-    X=(${X//:/ })
+    X=(${7//:/ })
     for x in "${X[@]}"; do
         EXCLUDES+=('--exclude')
         EXCLUDES+=("/$x")
@@ -30,7 +30,7 @@ git checkout $BRANCH
 
 # Sync $TARGET folder to $REPO state repository with excludes
 echo "running 'rsync -avh --delete "${EXCLUDES[@]}" $GITHUB_WORKSPACE/$TARGET/ $TEMP'"
-rsync -avh --delete "${EXCLUDES[@]}" $GITHUB_WORKSPACE/$TARGET/ $TEMP
+rsync -avh --delete "${EXCLUDES[@]}" $GITHUB_WORKSPACE/$SOURCE/ $TEMP/$TARGET
 
 # Success finish early if there are no changes
 if git diff --no-ext-diff --quiet; then
